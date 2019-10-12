02edaa6d2e021693953d949038f0002172c89b8a
Prasanna <prasanna@Prasannas-MacBook-Pro.local>
2019-10-12 23:55:03 +0530

    Updates

---
+++ precache-manifest.0a56769091607962172820aded5f59ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03b1786955bff68ea5a009c98f943993",
+    "url": "/TodoApp-React/index.html"
+  },
+  {
+    "revision": "4d749a8e87460e0db9ba",
+    "url": "/TodoApp-React/static/css/main.507336ad.chunk.css"
+  },
+  {
+    "revision": "fd6021ffed5a9e2e7df6",
+    "url": "/TodoApp-React/static/js/2.269c3438.chunk.js"
+  },
+  {
+    "revision": "4d749a8e87460e0db9ba",
+    "url": "/TodoApp-React/static/js/main.42dbb43c.chunk.js"
+  },
+  {
+    "revision": "674b43f2bfcdd94704bb",
+    "url": "/TodoApp-React/static/js/runtime-main.3df86df9.js"
+  }
+]);
